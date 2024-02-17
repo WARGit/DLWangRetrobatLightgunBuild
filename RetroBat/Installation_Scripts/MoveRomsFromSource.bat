573af9bacac86c5c0b13388fca652817fb645ac1
Wayne Robinson <wayne@sccm-consultants.com>
2024-02-17 16:21:58 +0000

    move windows roms out of source (UNTESTED)

--- RetroBat/Installation_Scripts/MoveRomsFromSource.bat
+++ RetroBat/Installation_Scripts/MoveRomsFromSource.bat
@@ -22,8 +22,28 @@ move /Y "%~dp0bios" "%directory%"
 REM === Create Roms\Windows dir in destination ===
 mkdir "%directory%\roms\windows"
 
-REM === Move Roms out of source here..... ====
-rem move /Y "%~dp0roms\windows" "%directory"
+REM === Move Windows Roms out of source ====
+move /Y "%~dp0roms\windows\Banzai Escape 2.PC" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Banzai Escape.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Big Buck Hunter Arcade.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Block King Ball Shooter.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Blue Estaet.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Dead Containment.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Death Live.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Demul 180428 Light Guns.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Dream Raiders.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Ed Hunter.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Elevator Action Death Parade.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Friction.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Fruit Ninja.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Gaia Attack 4.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Gal.Gun.Double.Peace.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Gundam SOZ.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Haunted Museum 2.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\Haunted Museum.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\House  Of The Dead Overkill.pc" "%directory\roms\windows"
+move /Y "%~dp0roms\windows\House Of The Dead Scarlet Dawn.pc" "%directory\roms\windows"
+
 pause
 
 
