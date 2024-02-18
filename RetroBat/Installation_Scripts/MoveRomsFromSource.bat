529c775b6e173108c9a0108b237702af7ddb1e34
Wayne Robinson <wayne@sccm-consultants.com>
2024-02-18 17:25:30 +0000

    move ps2 and ps3 items such as save states and cache and all Dev_ folders. dev_hdd0 will not move, access denied error, suspect $lock folder perhaps causing issue

--- RetroBat/Installation_Scripts/MoveRomsFromSource.bat
+++ RetroBat/Installation_Scripts/MoveRomsFromSource.bat
@@ -312,27 +312,26 @@ REM - TODO There a number of save states in the "sstates" folder, TBC if these a
 REM === Move xbox PS2 bios files out of source ====
 move /Y "%~dp0roms\windows\PCSX2 1.6.0.pc\bios" "%directory%\roms\windows\PCSX2 1.6.0.pc"
 
+REM === Move other files/folders out of PS2 emu source ====
+move /Y "%~dp0roms\windows\PCSX2 1.6.0.pc\sstates" "%directory%\roms\windows\PCSX2 1.6.0.pc"
+
 REM === Create RPCS3 dir in destination ===
 mkdir "%directory%\roms\windows\RPCS3.pc"
-mkdir "%directory%\roms\windows\RPCS3.pc\dev_hdd0"
-mkdir "%directory%\roms\windows\RPCS3.pc\dev_hdd0\disc"
-mkdir "%directory%\roms\windows\RPCS3.pc\dev_hdd0\game"
 
-REM === Move PS3 Roms out of source ====
-move /Y "%~dp0roms\windows\RPCS3.pc\dev_hdd0\disc\Time Crisis Razing Storm BLUS30528" "%directory%\roms\windows\RPCS3.pc\dev_hdd0\disc"
-move /Y "%~dp0roms\windows\RPCS3.pc\dev_hdd0\game\BLUS30528_DSP" "%directory%\roms\windows\RPCS3.pc\dev_hdd0\game"
-move /Y "%~dp0roms\windows\RPCS3.pc\dev_hdd0\game\BLUS30528" "%directory%\roms\windows\RPCS3.pc\dev_hdd0\game"
-move /Y "%~dp0roms\windows\RPCS3.pc\dev_hdd0\game\BLUS30093" "%directory%\roms\windows\RPCS3.pc\dev_hdd0\game"
-REM - BLUS30528 = Time Crisis Razing Storm
-REM - BLUS30093 = Time Crisis 4
+REM === Move files/folders out of PS3 emu source ====
+move /Y "%~dp0roms\windows\RPCS3.pc\dev_bdvd" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\dev_flash" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\dev_flash2" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\dev_flash3" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\dev_hdd0" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\dev_hdd1" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\dev_usb000" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\savestates" "%directory%\roms\windows\RPCS3.pc"
+move /Y "%~dp0roms\windows\RPCS3.pc\cache" "%directory%\roms\windows\RPCS3.pc"
 
 REM === Remove .git folder from RPCS3.pc ===
 rmdir /S /Q "%~dp0roms\windows\RPCS3.pc\git"
 REM - there is also a dir "rpcs3_old", presume this is not required - TBC
-REM - there is also a dir "dev_hdd1\caches", this contains cache files for Tekken by the look of it, probably not required - TBC
-REM - there is also a cache folder at the root of the RPCS3.pc dir - not sure what is required from here - TBC
-REM - there is also a savestates folder at the root of the RPCS3.pc dir - not sure what is required from here - TBC
-
 
 REM === Create Roms\xbox dir in destination ===
 mkdir "%directory%\roms\xbox"
